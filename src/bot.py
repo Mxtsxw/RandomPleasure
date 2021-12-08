ff12f5769a680bf64229a84e622fdb824d0edab2
Matthieu Randriantsoa <randriantsoamatthieu@gmail.com>
2021-12-08 21:33:04 +0100

    Creation and setup of virtual environment

--- src/bot.py
+++ src/bot.py
@@ -1,25 +1,40 @@
 # bot.py
-import os
 
+# Imports
+import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
+# Environment variable
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
+# Client and Bot setup
 client = discord.Client()
+bot = commands.Bot(command_prefix=['!', '.', '$'])
+
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f"RamdomPH Logged as {client.user.name}")
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game("vaut mieux pas savoir"))
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+
+@bot.command(name="greeting")
+async def greeting(ctx, member: discord.Member = None):
+    print('greeting launch')
+    if member:
+        await ctx.send(f'Hi {member.name} !')
+    else:
+        await ctx.send(f'Hi {ctx.author.name} !')
+
+
+@bot.command(name='video')
+async def video(ctx):
+    
+    print("Command run")
+    await ctx.send("A special gift for you")
 
 client.run(TOKEN)
 
